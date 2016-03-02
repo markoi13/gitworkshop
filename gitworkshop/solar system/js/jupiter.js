7b9eed216e2708100c68aebbbc8b53eba970ffc6
ivma <marko.ivanovic@zuehlke.com>
2016-03-02 03:48:19 +0100

    I have discovered Jupiter, Saturn, Uranus and Neptune

---
+++ gitworkshop/solar system/js/jupiter.js
@@ -0,0 +1,13 @@
+var planetInfo = {name: 'Jupiter', diameter: '139,822 km', orbitTime: '11.9 Earth years'}
+
+function getJupiterName() {
+	return planetInfo.name;
+}
+
+function getJupiterDiameter(){
+	return planetInfo.diameter;
+}
+
+function getJupiterOrbitTime() {
+	return planetInfo.orbitTime;
+}
