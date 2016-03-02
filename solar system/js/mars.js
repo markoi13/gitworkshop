3673ca918adf6ac4f6643a9c5aa2d13857b162ae
ivma <marko.ivanovic@zuehlke.com>
2016-03-02 03:30:23 +0100

    Discovered more planets

---
+++ solar system/js/mars.js
@@ -0,0 +1,13 @@
+var planetInfo = {name: 'Mars', diameter: '6,787 km', orbitTime: '687 Earth days'}
+
+function getMarsName() {
+	return planetInfo.name;
+}
+
+function getMarsDiameter(){
+	return planetInfo.diameter;
+}
+
+function getMarsOrbitTime() {
+	return planetInfo.orbitTime;
+}
---
+++ solar system/js/venus.js
@@ -0,0 +1,13 @@
+var planetInfo = {name: 'Venus', diameter: '12,104 km', orbitTime: '225 Earth days'}
+
+function getVenusName() {
+	return planetInfo.name;
+}
+
+function getVenusDiameter(){
+	return planetInfo.diameter;
+}
+
+function getVenusOrbitTime() {
+	return planetInfo.orbitTime;
+}
