9fedc6876b8ef575859032adaa9e59722ac94a0f
ivma <marko.ivanovic@zuehlke.com>
2016-03-02 13:37:37 +0100

    I have edited file solar system/js/pluto.js with JGit

--- solar system/js/pluto.js
+++ solar system/js/pluto.js
@@ -1,3 +1,25 @@
 
 ////This is commited with JGit at Wed Mar 02 13:23:04 CET 2016
 ////This is commited with JGit at Wed Mar 02 13:37:07 CET 2016
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+testingit
