891c20a696a4155da20779ff19755c3505ad2388
Bogdan Doroslovac <bogdan.doroslovac@zuehlke.com>
2016-10-12 11:34:06 +0200

    Discovered more planets

---
+++ solar system/js/earth.js
@@ -0,0 +1,13 @@
+﻿var planetInfo = {name: 'Earth', diameter: '12,760 km', orbitTime: '365 days'}
+
+function getEarthName() {
+   return planetInfo.name;
+}
+
+function getEarthDiameter(){
+   return planetInfo.diameter;
+}
+
+function getEarthOrbitTime() {
+   return planetInfo.orbitTime;
+}
