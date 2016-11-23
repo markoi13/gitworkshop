a48bff1d300526c446e72836ed2bf9dc18a33c2b
Djordje Madic <madj@zuehlke.com>
2016-11-23 13:00:50 +0100

    Discovered more planets

---
+++ solar system/js/earth.js
@@ -0,0 +1,17 @@
+/**
+ * Created by madj on 23.11.2016..
+ */
+
+var planetInfo = {name: 'Earth', diameter: '12,760 km', orbitTime: '365 days'}
+
+function getEarthName() {
+    return planetInfo.name;
+}
+
+function getEarthDiameter(){
+    return planetInfo.diameter;
+}
+
+function getEarthOrbitTime() {
+    return planetInfo.orbitTime;
+}
---
+++ solar system/js/mars.js
@@ -0,0 +1,17 @@
+/**
+ * Created by madj on 23.11.2016..
+ */
+
+var planetInfo = {name: 'Mars', diameter: '6,787 km', orbitTime: '687 Earth days'}
+
+function getMarsName() {
+    return planetInfo.name;
+}
+
+function getMarsDiameter(){
+    return planetInfo.diameter;
+}
+
+function getMarsOrbitTime() {
+    return planetInfo.orbitTime;
+}
