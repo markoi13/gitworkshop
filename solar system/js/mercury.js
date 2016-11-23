a45fbc729ec8ac33aa9b1b72549d24aa246a5628
Djordje Madic <madj@zuehlke.com>
2016-11-23 12:18:20 +0100

    I have discovered Mercury guys

---
+++ solar system/js/mercury.js
@@ -0,0 +1,17 @@
+/**
+ * Created by madj on 23.11.2016..
+ */
+
+var planetInfo = {name: 'Mercury', diameter: '4,878 km', orbitTime: '88 Earth days'}
+
+function getMercuryName() {
+    return planetInfo.name;
+}
+
+function getMercuryDiameter(){
+    return planetInfo.diameter;
+}
+
+function getMercuryOrbitTime() {
+    return planetInfo.orbitTime;
+}
