7512e7233a1a400cdaeeff5de1ba53e60bea6e79
Marko Oklobdžija <marko.oklobdzija@zuehlke.com>
2017-04-21 16:51:03 +0200

    I have discovered Mercury guys

---
+++ solar system/js/mercury.js
@@ -0,0 +1,21 @@
+var planetInfo =
+    {
+        name: 'Mercury',
+        diameter: '4,878 km',
+        orbitTime: '88 Earth days'
+    }
+
+function getMercuryName()
+{
+    return planetInfo.name;
+}
+
+function getMercuryDiameter()
+{
+    return planetInfo.diameter;
+}
+
+function getMercuryOrbitTime()
+{
+    return planetInfo.orbitTime;
+}
