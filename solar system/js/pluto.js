2dfba9a05ccde133af93afa6dbfab362fbf0ba03
tomcat <tomcat@ip-172-31-7-132.eu-central-1.compute.internal>
2017-07-18 12:18:46 +0000

    I have discovered Jupiter, Saturn, Uranus and Neptune

---
+++ solar system/js/pluto.js
@@ -0,0 +1,13 @@
+var planetInfo = {name: 'Pluto', diameter: 'N/A', orbitTime: '248 Earth years'}
+
+function getPlutoName() {
+	return planetInfo.name;
+}
+
+function getPlutoDiameter(){
+	return planetInfo.diameter;
+}
+
+function getPlutoOrbitTime() {
+	return planetInfo.orbitTime;
+}
