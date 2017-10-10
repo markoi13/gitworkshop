d7425493930e658855f50fe453319a81d403a302
tomcat <tomcat@ip-172-31-7-132.eu-central-1.compute.internal>
2017-10-10 10:50:03 +0000

    I have discovered Jupiter, Saturn, Uranus and Neptune

---
+++ solar system/js/uranus.js
@@ -0,0 +1,13 @@
+var planetInfo = {name: 'Uranus', diameter: '51,120 km', orbitTime: '84 Earth years'}
+
+function getUranusName() {
+	return planetInfo.name;
+}
+
+function getUranusDiameter(){
+	return planetInfo.diameter;
+}
+
+function getUranusOrbitTime() {
+	return planetInfo.orbitTime;
+}
