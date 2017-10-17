56d43682a5e032d844b06a74e1793c9f4cd17166
nandric <neda.andric@zuehlke.com>
2017-10-17 16:22:08 +0200

    Discovered more planets

---
+++ solar system/js/earth.js
@@ -0,0 +1,13 @@
+var planetInfo = {name: 'Earth', diameter: '12.760 km', orbitTime: '365'};
+
+function getEarthName(){
+	return planterInfo.name;
+}
+
+function getEarthDiameter(){
+	return planetInfo.diameter;
+}
+
+function getEarthOrbitTime(){
+	return planetInfo.orbitTime;
+}
